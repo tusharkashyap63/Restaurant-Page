d75d3834a7e97ff7dd0a76761e22f5b560416eb8
Tushar Kashyap <kashyap.tushar15@gmail.com>
2020-06-28 22:05:29 +0530

    Added items to menu page

--- src/contact.js
+++ src/contact.js
@@ -1,4 +1,14 @@
 export function contact() {
   const para = document.querySelector('p');
-  para.innerHTML = 'Contact Form';
+  para.innerHTML = `
+  <form>
+  <label for="name">Name:</label>
+  <input type="text" name="name" id="name" /><br>
+  <label for="contact">Contact Number:</label>
+  <input type="tel" name="contact" id="contact" /><br>
+  <label for="feedback">Your feedback here:</label>
+  <div>
+  <textarea id="feedback" name="feedback" rows="12"></textarea>
+  </div>
+  </form>`;
 }
--- src/menu.js
+++ src/menu.js
@@ -1,6 +1,20 @@
 export function menu() {
   const para = document.querySelector('p');
-  para.innerHTML = 'Menu here';
+  para.innerHTML = `
+  <div class="menu">
+      <div class="card">
+      <img src="burger.gif" alt="" class="card-img">
+      <p class="card-text">lafdjshnkslajblbk</p>
+    </div>
+    <div class="card">
+      <img src="burger.gif" alt="" class="card-img">
+      <p class="card-text">lafdjshnkslajblbk</p>
+    </div>
+    <div class="card">
+      <img src="burger.gif" alt="" class="card-img">
+      <p class="card-text">lafdjshnkslajblbk</p>
+    </div>
+  </div>`;
 
   let listItems = document.querySelectorAll('li');
   console.log(listItems);
